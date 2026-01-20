83302a0a5ec0cb2f9409f16488c5480421c000fd
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-01-20 09:48:12 +0100

    Ajout du nombre de personnes participant

--- voyage.js
+++ voyage.js
@@ -10,17 +10,23 @@ fetch('data.json')
             <img src="${item.image}" class="card-img-top" alt="${item.nom}">
             <div class="card-body">
               <h5 class="card-title">${item.nom}</h5>
-              <p class="">A partir de ${item.prix} €</p>
-              <button type="button" class="btn btn-primary rounded-pill">
+              <p>A partir de ${item.prix} €</p>
+
+              <!-- Ligne chiffre + bouton -->
+              <div class="d-flex justify-content-between align-items-center">
+                <span class="fw-bold">${item.personnes} Pers</span>
+                <button type="button" class="btn btn-primary rounded-pill">
                   Ajouter au panier
-              </button>
-              <p class="card-text">${item.description} €</p>
+                </button>
+              </div>
+
+              <p class="card-text mt-2">${item.description}</p>
             </div>
           </div>
         </div>
       `;
+
       container.innerHTML += card;
     });
   })
   .catch(error => console.error('Erreur chargement données:', error));
-
